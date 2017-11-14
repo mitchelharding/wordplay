620a4ea7fe686be51f69c213f5a67694926e1cf2
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-13 21:08:54 -0700

    Added structure to HTML and Script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $("form#form-group").submit(function(event){
+
+    var userWords = [ ];
+
+    userWords.forEach(function(userWord){
+
+
+    });
+    alert(userInput);
+    event.preventDefault();
+  });
+});
